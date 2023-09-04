5fe6f75daf4326a760a23650bb37ba96968a1c21
SeoJungHan <seojh5939@gmail.com>
2023-09-04 16:23:36 +0900

    [DESIGN] : 🎨 디자인 배치 완료

--- app/src/main/java/com/example/contact_nb12/main/MainActivity.kt
+++ app/src/main/java/com/example/contact_nb12/main/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.contact_nb12.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.contact_nb12.databinding.ActivityMainBinding
+import com.example.contact_nb12.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = MainActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
