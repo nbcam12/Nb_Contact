38901a5e465ddba40c15bfc1475d94f8d6eecb24
4seokhyeon <tktjrgus@naver.com>
2023-09-05 09:41:46 +0900

    [FEAT] : ⚙️ 마이페이지 수정 visible 적용

--- app/src/main/java/com/example/contact_nb12/mypage/AddContactDialogFragment.kt
+++ app/src/main/java/com/example/contact_nb12/mypage/AddContactDialogFragment.kt
@@ -1,18 +1,37 @@
 package com.example.contact_nb12.mypage
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.example.contact_nb12.R
 
-class AddContactDialogFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        return inflater.inflate(R.layout.fragment_add_contact_dialog, container, false)
+class AddContactDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle("연락처 추가")
+                .setMessage("여기에 연락처 추가 폼을 정의하세요.")
+                .setPositiveButton("확인") { dialog, _ ->
+                    // "확인" 버튼을 눌렀을 때의 동작을 여기에 추가
+                    // 예를 들어, 입력된 데이터를 가져와서 연락처를 추가하는 작업을 수행합니다.
+                    dialog.dismiss() // 다이얼로그 닫기
+                }
+                .setNegativeButton("취소") { dialog, _ ->
+                    // "취소" 버튼을 눌렀을 때의 동작을 여기에 추가
+                    dialog.dismiss() // 다이얼로그 닫기
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
     }
-
-
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/contact_nb12/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/contact_nb12/mypage/MyPageFragment.kt
@@ -1,17 +1,44 @@
 package com.example.contact_nb12.mypage
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.contact_nb12.R
+import com.example.contact_nb12.databinding.FragmentMyPageBinding
+import com.example.contact_nb12.main.MainActivity
 
 class MyPageFragment : Fragment() {
+    private var mainActivity: MainActivity? = null
+    private lateinit var binding: FragmentMyPageBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view =  inflater.inflate(R.layout.fragment_my_page, container, false)
-        return view
+        binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is MainActivity){
+            mainActivity = context
+        }
+    }
 
+    override fun onPause() {
+        super.onPause()
+        val editButton = mainActivity?.getEditButton()
+        editButton?.visibility = View.INVISIBLE
+    }
+    override fun onResume() {
+        super.onResume()
+        val editButton = mainActivity?.getEditButton()
+        editButton?.visibility = View.VISIBLE
+
+        editButton?.setOnClickListener{
+            val dialogFragment = AddContactDialogFragment() // 다이얼로그 프래그먼트 인스턴스 생성
+            dialogFragment.show(parentFragmentManager, "AddContactDialogFragment") // 다이얼로그 띄우기
+        }
+    }
 }
