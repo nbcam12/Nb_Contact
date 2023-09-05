2e1f293996986470e0d5dab32607898c973bd8e5
minji05 <smj95133@likelion.org>
2023-09-05 15:49:00 +0900

    [FEAT] : ⚙️ 연락처 리스트 intent 처리

--- app/src/main/java/com/example/contact_nb12/list/ContactListFragment.kt
+++ app/src/main/java/com/example/contact_nb12/list/ContactListFragment.kt
@@ -1,6 +1,8 @@
 package com.example.contact_nb12.list
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contact_nb12.R
 import com.example.contact_nb12.databinding.FragmentContactListBinding
+import com.example.contact_nb12.detail.DetailActivity
 import com.example.contact_nb12.models.Contact
 
 class ContactListFragment : Fragment() {
@@ -30,7 +33,25 @@ class ContactListFragment : Fragment() {
         val layoutManager = LinearLayoutManager(requireContext())
         binding.contactRecycler.layoutManager = layoutManager
 
+
+        // RecyclerView 아이템 클릭 이벤트 처리
+        adapter.itemClick = object : ContactAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val selectedItem = contacts[position]
+
+                val intent = Intent(requireContext(), DetailActivity::class.java)
+                intent.putExtra("selectedItem", selectedItem) // 객체를 intent에 추가
+                intent.putExtra("position", position)
+                Log.d("tag", selectedItem.toString())
+                startActivityForResult(intent, REQUEST_DETAIL) // DetailActivity 시작
+            }
+        }
+
         return binding.root
     }
+    companion object {
+        private const val REQUEST_DETAIL = 101
+    }
+
 }
 
