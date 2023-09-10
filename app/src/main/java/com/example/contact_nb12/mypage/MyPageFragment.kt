cee633265fa391f38d215e7f5e15d6cad198782e
SeoJungHan <seojh5939@gmail.com>
2023-09-11 01:45:36 +0900

    [FEAT] : ⚙ 이미지처리 Glide로 변경

--- app/src/main/java/com/example/contact_nb12/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/contact_nb12/mypage/MyPageFragment.kt
@@ -7,7 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.example.contact_nb12.R
+import com.example.contact_nb12.Util.Utils.getDefaultImgUri
+import com.example.contact_nb12.Util.Utils.getImageForUri
 import com.example.contact_nb12.databinding.FragmentMyPageBinding
 import com.example.contact_nb12.detail.ContactDetailFragment
 import com.example.contact_nb12.main.MainActivity
@@ -20,19 +25,8 @@ class MyPageFragment : Fragment() {
     private val editButton by lazy {
         MainActivity.instance.getEditButton()
     }
-
-//    private val item by lazy {
-//        arguments?.let{
-//            Contact(
-//                Img = Uri.parse(it.getString(ContactDetailFragment.BUNDLE_IMAGE) ?: getDefaultImgUri().toString()),
-//                name = it.getString(ContactDetailFragment.BUNDLE_NAME) ?: "",
-//                phonenumber = it.getString(ContactDetailFragment.BUNDLE_NUMBER) ?: "",
-//                email = it.getString(ContactDetailFragment.BUNDLE_EMAIL) ?: "",
-//                birth = it.getString(ContactDetailFragment.BUNDLE_BIRTH)?: "",
-//                nickname = it.getString(ContactDetailFragment.BUNDLE_NICKNAME)?: "",
-//            )
-//        }
-//    }
+    // myPage -> EditContactDialog 전달용
+    private var profile_image : Uri? = null
 
     companion object{
         const val ENTRY_POINT_MYPAGE = -99
@@ -50,11 +44,9 @@ class MyPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
-        initData()
     }
 
     private fun initView() {
-
         editButton.setOnClickListener {
             val item = packagingData()
             val dialogFragment = EditContactDialogFragment(
@@ -67,7 +59,8 @@ class MyPageFragment : Fragment() {
 
     fun updateData(item: Contact)= with(binding) {
         item.let{
-            profileImage.setImageURI(it.Img)
+            profile_image = it.Img!!
+            getImageForUri(requireContext(), it.Img, profileImage)
             profileName.text = it.name
             profilePhone.text = it.phonenumber
             profileBirth.text = it.birth
@@ -76,36 +69,15 @@ class MyPageFragment : Fragment() {
         }
     }
 
-    private fun initData()= with(binding){
-//        item?.let {
-//            profileImage.setImageURI(it.Img)
-//            profileName.text = it.name
-//            profilePhone.text = it.phonenumber
-//            profileBirth.text = it.birth
-//            profileEmail.text = it.email
-//            profileNickname.text = it.nickname
-//        }
-    }
-
     private fun packagingData(): Contact = with(binding) {
         return Contact(
-            Img = getDefaultImgUri(),
+            Img = profile_image ?: getDefaultImgUri(requireContext()),
             name = profileName.text.toString(),
             phonenumber = profilePhone.text.toString(),
             birth = profileBirth.text.toString(),
             email = profileEmail.text.toString(),
             nickname = profileNickname.text.toString()
         )
-    }
-
-    // 기본프로필이미지 Uri
-    private fun getDefaultImgUri(): Uri {
-        val resId = R.drawable.dialog_profile
-        val imgUri =
-            "${ContentResolver.SCHEME_ANDROID_RESOURCE}://${resources.getResourcePackageName(resId)}/${
-                resources.getResourceTypeName(resId)
-            }/${resources.getResourceEntryName(resId)}"
-        return Uri.parse(imgUri)!!
     }
 
     override fun onPause() {
